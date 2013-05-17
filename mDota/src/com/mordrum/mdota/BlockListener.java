85e537fff810536097f1efe524848c55506aa61c
Jesse Savary <jesse.savary@live.com>
2013-05-16 20:49:33 -0400

    Cleaned up code, fixed a bunch of bugs, fixed colored player name functionality, stripped multiverse dependency

---
+++ mDota/src/com/mordrum/mdota/BlockListener.java
@@ -0,0 +1,79 @@
+package com.mordrum.mdota;
+
+import org.bukkit.Material;
+import org.bukkit.block.BlockFace;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Monster;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockDamageEvent;
+import org.bukkit.event.block.BlockDispenseEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.material.Dispenser;
+import org.bukkit.material.SpawnEgg;
+
+public class BlockListener implements Listener {
+	DotaMinecraft plugin = null;
+
+	public BlockListener(DotaMinecraft plugin) {
+		this.plugin = plugin;
+	}
+
+	@EventHandler
+	public void onBlockBreak(BlockBreakEvent event) {
+		if (event.getBlock().getWorld().getName().equals(plugin.WorldName)) {
+			event.setCancelled(true);
+		}
+	}
+
+	@EventHandler
+	public void onBlockDamage(BlockDamageEvent event) {
+		if (event.getBlock().getWorld().getName().equals(plugin.WorldName)) {
+			event.setCancelled(true);
+		}
+	}
+
+	@EventHandler
+	public void onBlockPlace(BlockPlaceEvent event) {
+		if (event.getBlock().getWorld().getName().equals(plugin.WorldName)) {
+			event.setCancelled(true);
+		}
+	}
+
+	@EventHandler
+	public void onBlockDispenseEvent(BlockDispenseEvent event) {
+		if (event.getBlock().getWorld().getName().equals(plugin.WorldName)) {
+			if (event.getItem().getType() == Material.MONSTER_EGG) {
+				Dispenser disp = (Dispenser) event.getBlock().getState().getData();
+				SpawnEgg e = (SpawnEgg) event.getItem().getData();
+				BlockFace face = disp.getFacing();
+				if (face == BlockFace.EAST) {
+					face = BlockFace.NORTH;
+				} else if (face == BlockFace.SOUTH) {
+					face = BlockFace.EAST;
+				} else if (face == BlockFace.WEST) {
+					face = BlockFace.SOUTH;
+				} else if (face == BlockFace.NORTH) {
+					face = BlockFace.WEST;
+				}
+				LivingEntity newEntity = (LivingEntity) event.getBlock().getWorld().spawnEntity(
+						event.getBlock().getRelative(face).getLocation().add(.5, 0., .5),
+						e.getSpawnedType());
+				if (plugin.removeMobArmor) {
+					newEntity.getEquipment().setHelmet(null);
+					newEntity.getEquipment().setChestplate(null);
+					newEntity.getEquipment().setLeggings(null);
+					newEntity.getEquipment().setBoots(null);
+				}
+				if (plugin.giveMobsHelmet) {
+					if (newEntity instanceof Monster && (newEntity.getEquipment().getHelmet() == null || newEntity.getEquipment().getHelmet().getType() == Material.AIR)) {
+						newEntity.getEquipment().setHelmet(new ItemStack(Material.LEATHER_HELMET));
+					}
+				}
+				event.setCancelled(true);
+			}
+		}
+	}
+}
---
+++ mDota/src/com/mordrum/mdota/RecallScheduler.java
@@ -0,0 +1,27 @@
+package com.mordrum.mdota;
+
+import org.bukkit.entity.Player;
+
+public class RecallScheduler
+		implements Runnable {
+	String name;
+	DotaMinecraft plugin;
+
+	public RecallScheduler(DotaMinecraft plugin, String playername) {
+		this.plugin = plugin;
+		this.name = playername;
+	}
+
+	public void run() {
+		Player player = this.plugin.getServer().getPlayer(this.name);
+		Integer team = this.plugin.playerlist.get(this.name);
+		if (team == 1)
+			player.teleport(this.plugin.RedPoint);
+		else if (team == 2)
+			player.teleport(this.plugin.BluePoint);
+		else {
+			player.sendMessage("How can you recall if you're not on a team?");
+		}
+		this.plugin.playerRecallID.remove(this.name);
+	}
+}
