8745deea352c519f4bf7a6b45a15a3431a94a72e
SirSavary <jesse.savary@live.com>
2013-06-17 18:18:18 -0400

    Added more prizes More efficient code Removed some dead code

--- mCommon/src/com/mordrum/mcommon/reward/PrizeWheel.java
+++ mCommon/src/com/mordrum/mcommon/reward/PrizeWheel.java
@@ -5,6 +5,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.regex.Pattern;
 
@@ -16,40 +17,47 @@ import static org.bukkit.Material.*;
  * Date: 3/2/13
  * Time: 7:06 PM
  */
-public class PrizeWheel { //TODO finish prizewheel API
+public class PrizeWheel {
 
-	int randomRange = 100; //Range of numbers to use
-	ArrayList<ItemStack> prizeArray;
+	static int randomRange = 100; //Range of numbers to use
+    static ItemStack[] prizes;
 
-	public PrizeWheel() {
-		prizeArray = new ArrayList<ItemStack>();
+    public static void SeedPrizes() {
+        for (int i = 0; 1 < randomRange; i++) {
+            //Common prizes
+            if (i < 10) prizes[i] = new ItemStack(COAL, 8);
+            else if(i < 20) prizes[i] = new ItemStack(IRON_INGOT, 4);
+            else if(i < 30) prizes[i] = new ItemStack(GOLD_INGOT, 2);
+            else if(i < 40) prizes[i] = new ItemStack(APPLE, 6);
+            else if(i < 50) prizes[i] = new ItemStack(DIAMOND, 1);
+            else if(i < 60) prizes[i] = new ItemStack(ENDER_PEARL, 3);
+            else if(i < 70) prizes[i] = new ItemStack(WATER_LILY, 5);
 
-		for (int a = 0; a < 100; a++) {
-			if (a < 101) {
-				if ((a % 10 == 0) || (a % 9 == 0)) prizeArray.add(new ItemStack(COAL, 8));
-				else if (a % 8 == 0) prizeArray.add(new ItemStack(IRON_INGOT, 4));
-				else if (a % 7 == 0) prizeArray.add(new ItemStack(GOLD_INGOT, 2));
-				else if (a % 6 == 0) prizeArray.add(new ItemStack(APPLE, 6));
-				else if (a % 5 == 0) prizeArray.add(new ItemStack(DIAMOND, 1));
-				else if (a % 4 == 0) prizeArray.add(new ItemStack(ENDER_PEARL, 3));
-				else if (a % 3 == 0) prizeArray.add(new ItemStack(WATER_LILY, 4));
-				else if (a % 2 == 0) prizeArray.add(new ItemStack(ARROW, 10));
-				else if (a % 1 == 0) prizeArray.add(new ItemStack(SLIME_BALL, 2));
-			} else {
+            //Uncommon prizes - 5%
+            else if(i < 75) prizes[i] = new ItemStack(SULPHUR, 6);
+            else if(i < 80) prizes[i] = new ItemStack(ARROW, 16);
+            else if(i < 85) prizes[i] = new ItemStack(SLIME_BALL, 3);
+            else if(i < 90) prizes[i] = new ItemStack(FIREBALL, 5);
 
-			}
-		}
-	}
+            //Rare prizes - 2% chance
+            else if(i < 92) prizes[i] = new ItemStack(DIAMOND_BLOCK, 1);
+            else if(i < 94) prizes[i] = new ItemStack(GOLD_BLOCK, 2);
+            else if(i < 96) prizes[i] = new ItemStack(LAPIS_BLOCK, 2);
 
-	public void Spin(Player p, int x) {
-		String preMessage = ChatColor.YELLOW + "Spinning the wheel...";
+            //Very rare prizes - 1% chance
+            else if(i == 96) prizes[i] = new ItemStack(MONSTER_EGG, 1, (short) 91); //Sheep spawner
+            else if(i == 97) prizes[i] = new ItemStack(MONSTER_EGG, 1, (short) 93); //Chicken spawner
+            else if(i == 98) prizes[i] = new ItemStack(MONSTER_EGG, 1, (short) 90); //Pig spawner
+            else if(i == 99) prizes[i] = new ItemStack(MONSTER_EGG, 1, (short) 92); //Cow spawner
+        }
+    }
 
+	public static void Spin(Player p, int x) {
 		Random generator = new Random();
-		ArrayList<ItemStack> itemsWon = new ArrayList<ItemStack>();
-		int r = 0;
+		List<ItemStack> itemsWon = new ArrayList<>();
 		for (int i = 0; i < x; i++) {
-			r = generator.nextInt(100);
-			ItemStack itemToAdd = prizeArray.get(r); //Get the item the player won for this spin
+			int r = generator.nextInt(randomRange);
+			ItemStack itemToAdd = prizes[r]; //Get the item the player won for this spin
 			if (!itemsWon.isEmpty()) {
 				Boolean wasFound = false;
 				for (ItemStack itemInLoop : itemsWon) {
@@ -63,17 +71,24 @@ public class PrizeWheel { //TODO finish prizewheel API
 				}
 			} else itemsWon.add(itemToAdd);
 		}
-		String winMessage = ChatColor.YELLOW + "You won: ";
+        StringBuilder sb = new StringBuilder();
+        sb.append(ChatColor.YELLOW);
+        sb.append("You won: ");
 		for (ItemStack item : itemsWon) {
 			p.getInventory().addItem(item);
 			String messageToAdd = "";
 			//If there is more than one item make it plural
 			if (item.getAmount() > 1) messageToAdd = item.getAmount() + " " + item.getType().name().toLowerCase() + "s";
 			//If this is the last item, make it grammatically correct
-			if (itemsWon.indexOf(item) == itemsWon.size() - 1) winMessage = winMessage + "and " + messageToAdd;
-			else winMessage = winMessage + messageToAdd + ", ";
+			if (itemsWon.indexOf(item) == itemsWon.size() - 1) {
+                sb.append("and ");
+                sb.append(messageToAdd);
+            }
+			else {
+                sb.append(messageToAdd);
+                sb.append(", ");
+            }
 		}
-		winMessage.replaceAll(Pattern.quote("_"), " ");
-		p.sendMessage(winMessage);
+		p.sendMessage(sb.toString().replaceAll(Pattern.quote("_"), " "));
 	}
 }
