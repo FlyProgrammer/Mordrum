76f0e3967bb10f6c566944fdb274b0e896cd3579
Jesse Savary <jesse.savary@live.com>
2013-06-20 11:42:27 -0400

    Changed kill method to something that should be safer and more stable

--- mCommon/src/com/mordrum/mcommon/misc/CreativeOverkillListener.java
+++ mCommon/src/com/mordrum/mcommon/misc/CreativeOverkillListener.java
@@ -1,6 +1,7 @@
 package com.mordrum.mcommon.misc;
 
 import org.bukkit.GameMode;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -15,14 +16,15 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent;
  */
 public class CreativeOverkillListener implements Listener {
 
-    @EventHandler
-    public void onPlayerDamage(EntityDamageByEntityEvent e) {
-        if (e.getDamager() instanceof Player) {
-            if (!(e.getEntity() instanceof Player)) {
-                if (((Player) e.getDamager()).getGameMode() == GameMode.CREATIVE) {
-                    e.setDamage(10000);
-                }
-            }
-        }
-    }
+	@EventHandler
+	public void onPlayerDamage(EntityDamageByEntityEvent e) {
+		if (e.getDamager() instanceof Player) {
+			if (!(e.getEntity() instanceof Player) && (e.getEntity() instanceof LivingEntity)) {
+				if (((Player) e.getDamager()).getGameMode() == GameMode.CREATIVE) {
+					LivingEntity le = (LivingEntity) e.getEntity();
+					le.setHealth(0);
+				}
+			}
+		}
+	}
 }
