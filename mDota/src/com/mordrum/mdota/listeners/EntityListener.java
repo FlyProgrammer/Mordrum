8724d3a535af5bf007b520a080ffe540588c9431
SirSavary <jesse.savary@live.com>
2013-06-29 12:01:47 -0400

    Entire class has been cleaned up and will now function with the new system

--- mDota/src/com/mordrum/mdota/listeners/EntityListener.java
+++ mDota/src/com/mordrum/mdota/listeners/EntityListener.java
@@ -2,7 +2,6 @@ package com.mordrum.mdota.listeners;
 
 import com.mordrum.mdota.mDota;
 import com.mordrum.mdota.util.DotaGame;
-import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
@@ -15,240 +14,165 @@ import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.event.player.*;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerPickupItemEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.PlayerInventory;
 
 public class EntityListener implements Listener {
-	mDota plugin;
-
-	//TODO this entire class D:::::
-	public EntityListener(mDota plugin) {
-		this.plugin = plugin;
-	}
 
     /*
     If the player attempts to enter the enemy's spawn, kill them
      */
-	@EventHandler
-	public void onPlayerMove(PlayerMoveEvent event) {
-		Location loc = event.getPlayer().getLocation();
-		Integer x = loc.getBlockX();
-		Integer z = loc.getBlockZ();
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent event) {
+        Location loc = event.getPlayer().getLocation();
+        Integer x = loc.getBlockX();
+        Integer z = loc.getBlockZ();
         Player player = event.getPlayer();
-		String playerName = player.getName();
+        String playerName = player.getName();
 
         DotaGame dg = mDota.activeGames.get(mDota.playerList.get(playerName));
         int teamID = dg.getPlayerTeam().get(playerName);
 
         if (teamID == 1 && mDota.blueSpawn.contains(x, z)) {
-           player.setHealth(0);
-           player.sendMessage(mDota.tag + "You have been killed for attempting to enter the enemy's spawn!");
-        }
-        else if (teamID == 2 && mDota.redSpawn.contains(x, z)) {
+            player.setHealth(0);
+            player.sendMessage(mDota.tag + "You have been killed for attempting to enter the enemy's spawn!");
+        } else if (teamID == 2 && mDota.redSpawn.contains(x, z)) {
             player.setHealth(0);
             player.sendMessage(mDota.tag + "You have been killed for attempting to enter the enemy's spawn!");
         }
-	}
+    }
 
-	@EventHandler
-	public void onEntityDamageByEntity(EntityDamageByEntityEvent e) {
-		if (e.getEntity() instanceof Player) {
-			if (e.getDamager() instanceof Player) {
+    /*
+    Makes sure that no friendly fire occurs
+     */
+    //TODO make sure this works for arrows
+    @EventHandler
+    public void onEntityDamageByEntity(EntityDamageByEntityEvent e) {
+        if (e.getEntity() instanceof Player) {
+            if (e.getDamager() instanceof Player) {
+                //Get player names and the game they are playing in
+                String attackerName = ((Player) e.getDamager()).getName();
+                String defenderName = ((Player) e.getEntity()).getName();
+                DotaGame dg = mDota.activeGames.get(attackerName);
 
-			}
-		}
+                //Check for friendly fire
+                int player1TeamID = dg.getPlayerTeam().get(attackerName);
+                int player2TeamID = dg.getPlayerTeam().get(defenderName);
+                if (player1TeamID == player2TeamID) e.setCancelled(true);
+            }
+        }
+    }
 
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        //Keep items on death if configured
+        if (mDota.retainItems) {
+            DamageCause cause = event.getEntity().getLastDamageCause().getCause();
+            if (cause == DamageCause.DROWNING) {
+                event.getEntity().sendMessage(mDota.tag + "You died in the pirate cave, so you lost your items!");
+            } else if (cause == DamageCause.LAVA) {
+                event.getEntity().sendMessage(mDota.tag + "You died in the lava cave, so you lost your items!");
+            } else {
+                //TODO code to make players keep items (Store the items until they respawn)
+            }
+            //Let players keep their levels on death if configured
+            event.setKeepLevel(mDota.retainLevels);
+            //Add a death to the player's score
+            String playerName = event.getEntity().getName();
+            DotaGame dg = mDota.activeGames.get(mDota.playerList.get(playerName));
+            dg.getPlayerDeaths().put(playerName, dg.getPlayerDeaths().get(playerName) + 1);
+        }
+    }
 
-		if (event.getDamager().getWorld().getName().equals(plugin.WorldName) && event.getDamager() instanceof Player && event.getEntity() instanceof Player) {
-			String p1 = ((Player) event.getEntity()).getName();
-			String p2 = ((Player) event.getDamager()).getName();
-			if (plugin.playerlist.containsKey(p1) && plugin.playerlist.containsKey(p2)) {
-				if (plugin.playerlist.get(p1) == plugin.playerlist.get(p2)) {
-					event.setCancelled(true);
-				}
-			} else {
-				event.setCancelled(true);
-			}
-		}
-	}
+    //TODO keep items on death code
+//	@EventHandler
+//	public void onPlayerRespawn(PlayerRespawnEvent event) {
+//
+//	}
 
-	@EventHandler
-	public void onPlayerDeath(PlayerDeathEvent event) {
-		if (!event.getEntity().getWorld().getName().equals(plugin.WorldName)) {
-			return;
-		}
-		if (plugin.PlayersKeepItems) {
-			DamageCause cause = event.getEntity().getLastDamageCause().getCause();
-			if (cause == DamageCause.DROWNING) {
-				event.getEntity().sendMessage("You died in the pirate cave, so you lost your items!");
-			} else if (cause == DamageCause.LAVA) {
-				event.getEntity().sendMessage("You died in the lava cave, so you lost your items!");
-			} else {
-				plugin.playerdeathitems.put(event.getEntity().getName(), event.getEntity().getInventory().getContents());
-				plugin.playerdeatharmor.put(event.getEntity().getName(), event.getEntity().getInventory().getArmorContents());
-//				event.getEntity().getInventory().clear();
-//				for (ItemStack e : event.getDrops()) {
-//					System.out.println(e.getType().name());
-//				}
-//				System.out.println(event.getDrops().size());
-				event.getDrops().clear();
-			}
-		}
-		event.setKeepLevel(plugin.PlayersKeepLevel);
-		String name = event.getEntity().getName();
-		if (plugin.playerdeaths.containsKey(name)) {
-			plugin.playerdeaths.put(name, plugin.playerdeaths.get(name) + 1);
-		}
-	}
+    @EventHandler
+    public void onEntityDeath(EntityDeathEvent event) {
+        //If a player kills something
+        if (event.getEntity().getKiller() != null) {
+            Player killer = event.getEntity().getKiller();
+            DotaGame dg = mDota.activeGames.get(mDota.playerList.get(killer.getName()));
+            //If a player kills a mob, put the items directly into the player's inventory
+            if (event.getEntityType() != EntityType.PLAYER) {
+                for (ItemStack item : event.getDrops()) {
+                    killer.getInventory().addItem(item);
+                }
+                dg.getPlayerCreepScore().put(killer.getName(), dg.getPlayerCreepScore().get(killer.getName()));
+                //Make sure the mob doesn't DROP anything onto the ground
+                event.getDrops().clear();
+            }
+            //Give the player a point for killing another player
+            else {
+                dg.getPlayerKills().put(killer.getName(), dg.getPlayerKills().get(killer.getName()) + 1);
+            }
+        }
+    }
 
-	@EventHandler
-	public void onPlayerRespawn(PlayerRespawnEvent event) {
-		if (!event.getPlayer().getWorld().getName().equals(plugin.WorldName)) {
-			return;
-		}
-		String name = event.getPlayer().getName();
-		if (plugin.playerdeathitems.containsKey(name)) {
-			PlayerInventory inven = event.getPlayer().getInventory();
-			Integer count = 0;
-			for (ItemStack istack : plugin.playerdeathitems.get(name)) {
-				if (istack instanceof ItemStack) {
-					inven.setItem(count, istack);
-				}
-				count++;
-			}
-			//inven.setContents(plugin.playerdeathitems.get(name));
-			inven.setArmorContents(plugin.playerdeatharmor.get(name));
-			plugin.playerdeathitems.remove(name);
-			plugin.playerdeatharmor.remove(name);
-		}
-	}
+    /*
+    Prevent players from picking up items in the enemy team's nexus
+     */
+    @EventHandler
+    public void onPlayerPickupItem(PlayerPickupItemEvent event) {
+        String playerName = event.getPlayer().getName();
+        DotaGame dg = mDota.activeGames.get(mDota.playerList.get(playerName));
+        if (dg.IsPlayerInEnemyNexus(event.getPlayer())) event.setCancelled(true);
+    }
 
-	@EventHandler
-	public void onEntityDeath(EntityDeathEvent event) {
-		if (!event.getEntity().getWorld().getName().equals(plugin.WorldName)) {
-			return;
-		}
-		if (event.getEntityType() != EntityType.PLAYER) {
-			event.getEntity().getEquipment().setHelmet(null);
-			event.getEntity().getEquipment().setChestplate(null);
-			event.getEntity().getEquipment().setLeggings(null);
-			event.getEntity().getEquipment().setBoots(null);
-			if (event.getEntity().getKiller() instanceof Player) {
-				Player player = event.getEntity().getKiller();
-				for (ItemStack istack : event.getDrops()) {
-					player.getInventory().addItem(istack);
-				}
-				if (plugin.playercs.containsKey(player.getName())) {
-					plugin.playercs.put(player.getName(), plugin.playercs.get(player.getName()) + 1);
-				}
-			}
-			event.getDrops().clear();
-		} else {
-			if (event.getEntity().getKiller() instanceof Player) {
-				String name = event.getEntity().getKiller().getName();
-				if (plugin.playerkills.containsKey(name)) {
-					plugin.playerkills.put(name, plugin.playerkills.get(name) + 1);
-				}
-			}
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        //If the player is right clicking a block
+        if (event.getAction() == Action.RIGHT_CLICK_BLOCK && event.hasBlock()) {
+            Material blockType = event.getClickedBlock().getType();
+            //If player is interacting with a dispenser, block it
+            if (blockType == Material.DISPENSER) {
+                event.setCancelled(true);
+            }
+            //If they're interacting with a lever, make sure the other team has enough players
+            else if (blockType == Material.LEVER) {
+                Player player = event.getPlayer();
+                DotaGame dg = mDota.activeGames.get(mDota.playerList.get(player.getName()));
+                if (dg.getTeam1().getCount() < 3 || dg.getTeam2().getCount() < 3) {
+                    event.setCancelled(true);
+                    player.sendMessage(mDota.tag + "Waiting for opposing players to join.");
+                }
+            }
+            //If they're interacting with a chest in the enemy's nexus, block it
+            else if (blockType == Material.CHEST) {
+                Player player = event.getPlayer();
+                DotaGame dg = mDota.activeGames.get(mDota.playerList.get(player.getName()));
+                if (dg.IsPlayerInEnemyNexus(player)) event.setCancelled(true);
+            }
+        }
+    }
 
-		}
-	}
+    @EventHandler
+    public void onEntityExplode(EntityExplodeEvent event) {
+        DotaGame dg = mDota.activeGames.get(mDota.playerList.get(event.getLocation().getWorld().getPlayers().get(0).getName()));
 
-	@EventHandler
-	public void onPlayerPickupItem(PlayerPickupItemEvent event) {
-		if (!event.getPlayer().getWorld().getName().equals(plugin.WorldName)) {
-			return;
-		}
-		String name = event.getPlayer().getName();
-		if (plugin.playerlist.containsKey(name)) {
-			if (plugin.playerlist.get(name) == 1) {
-				if (event.getItem().getLocation().distance(plugin.BluePoint) < 70.0) {
-					event.setCancelled(true);
-				}
-			} else if (plugin.playerlist.get(name) == 2) {
-				if (event.getItem().getLocation().distance(plugin.RedPoint) < 70.0) {
-					event.setCancelled(true);
-				}
-			}
-		}
-	}
+        Location exLoc = event.getLocation();
+        for (Location tuLoc : dg.turretLocations.keySet()) {
+            if (exLoc.distance(tuLoc) < 10) {
+                String turretName = dg.turretLocations.get(tuLoc);
+                if (!dg.getTurretState(turretName)) { //If turret is not already destroyed
+                    dg.BroadcastMessage(mDota.tag + turretName + " has been destroyed!");
+                    dg.setTurretstate(turretName, true);
 
-	@EventHandler
-	public void onPlayerInteract(PlayerInteractEvent event) {
-		if (!event.getPlayer().getWorld().getName().equals(plugin.WorldName)) {
-			return;
-		}
-		if (event.getAction() == Action.RIGHT_CLICK_BLOCK && event.hasBlock()) {
-			if (event.getClickedBlock().getType() == Material.DISPENSER) {
-				event.setCancelled(true);
-			} else if (event.getClickedBlock().getType() == Material.LEVER) {
-				if (plugin.RedCount == 0 || plugin.BlueCount == 0) {
-					event.setCancelled(true);
-					event.getPlayer().sendMessage("Waiting for opposing players to join.");
-				}
-			} else if (event.getClickedBlock().getType() == Material.CHEST) {
-				String name = event.getPlayer().getName();
-				if (plugin.playerlist.containsKey(name)) {
-					if (plugin.playerlist.get(name) == 1) {
-						if (plugin.playerlist.containsKey(name)) {
-							if (plugin.playerlist.get(name) == 1) {
-								if (event.getClickedBlock().getLocation().distance(plugin.BluePoint) < 70.0) {
-									event.setCancelled(true);
-								}
-							} else if (plugin.playerlist.get(name) == 2) {
-								if (event.getClickedBlock().getLocation().distance(plugin.BluePoint) < 70.0) {
-									event.setCancelled(true);
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-
-	@EventHandler
-	public void onEntityExplode(EntityExplodeEvent event) {
-		if (plugin.GameInProgress && event.getLocation().getWorld().getName().equals(plugin.WorldName)) {
-			Location exloc = event.getLocation();
-			for (Location tuloc : plugin.turretlocations.keySet()) {
-				if (tuloc.distance(exloc) < 10) {
-					String turretname = plugin.turretlocations.get(tuloc);
-					if (plugin.turretstates.get(turretname) == false) {
-						plugin.broadcastMessage(ChatColor.RED + "[DOTA]" + turretname + " has been destroyed.");
-						plugin.turretstates.put(turretname, true);
-						if (turretname.equals("Red Nexus")) {
-							plugin.broadcastMessage(ChatColor.RED + "[DOTA]" + ChatColor.BLUE + "Blue Team has won!");
-							plugin.GameInProgress = false;
-							plugin.resetMap();
-						} else if (turretname.equals("Blue Nexus")) {
-							plugin.broadcastMessage(ChatColor.RED + "[DOTA]" + ChatColor.RED + "Red Team has won!");
-							plugin.GameInProgress = false;
-							plugin.resetMap();
-						}
-					}
-				}
-			}
-		}
-	}
-
-	@EventHandler
-	public void onPlayerTeleport(PlayerTeleportEvent event) {
-		if (!event.getTo().getWorld().getName().equals(plugin.WorldName)) {
-			String name = event.getPlayer().getName();
-			if (plugin.playerlist.containsKey(name) && event.getFrom().getWorld().getName().equals(plugin.WorldName)) {
-				plugin.playerlist.remove(name);
-				event.getPlayer().setPlayerListName(event.getPlayer().getName());
-				plugin.playerdeaths.remove(name);
-				plugin.playerkills.remove(name);
-				plugin.playerdeathitems.remove(name);
-				plugin.playerdeatharmor.remove(name);
-				event.getPlayer().getInventory().clear();
-				event.getPlayer().getInventory().setArmorContents(null);
-				event.getPlayer().setBedSpawnLocation(event.getTo().getWorld().getSpawnLocation(), true);
-				event.getPlayer().sendMessage(ChatColor.DARK_RED + "You have left the game.");
-			}
-		}
-	}
+                    if (turretName.endsWith("Nexus")) { //If the nexus was destroyed
+                        if (tuLoc.distance(dg.team1Point) < 70.0) {
+                            dg.BroadcastMessage(mDota.tag + dg.getTeam2().getName() + " has won!");
+                        } else if (tuLoc.distance(dg.team2Point) < 70.0) {
+                            dg.BroadcastMessage(mDota.tag + dg.getTeam1().getName() + " has won!");
+                        }
+                        dg.EndGame(); //Game has finished, end it
+                    }
+                }
+            }
+        }
+    }
 }
